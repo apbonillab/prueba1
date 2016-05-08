1552b41a8718c75b610ff62b110f529d1bf3bc30
Juan Caicedo <jjcc1421@gmail.com>
2016-05-07 23:04:52 -0500

    Mobile base version ready

--- public/js/ui/nav-toogle.js
+++ public/js/ui/nav-toogle.js
@@ -1,4 +1,23 @@
 document.querySelector("#nav-toggle")
     .addEventListener("click", function () {
         this.classList.toggle("active");
+        animate();
     });
+
+function animate() {
+    function animateIn() {
+        document.getElementById("navbar").className = "animated-in";
+        document.getElementById("container").className = "translucent-in";
+    }
+    function animateOut() {
+        document.getElementById("navbar").className = "animated-out";
+        document.getElementById("container").className = "";
+    }
+    if (document.getElementById("navbar").className === "animated-in") {
+        animateOut();
+    }
+    else {
+        animateIn();
+    }
+}
+
