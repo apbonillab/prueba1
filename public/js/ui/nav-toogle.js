d23152bba2b96bf412710deff449fed60c9b9d3b
Juan Caicedo <jjcc1421@gmail.com>
2016-05-10 00:20:58 -0500

    Add left efect

--- public/js/ui/nav-toogle.js
+++ public/js/ui/nav-toogle.js
@@ -12,11 +12,14 @@ function animate() {
         document.getElementById("navbar").className = "animated-in";
         document.getElementById("container").className = "translucent-in";
         document.getElementById("logo-mobile").className = "nav-logo-in";
+        document.getElementById("mask-container").className = "mask";
+
     }
     function animateOut() {
         document.getElementById("navbar").className = "animated-out";
         document.getElementById("container").className = "";
         document.getElementById("logo-mobile").className = "nav-logo-out";
+        document.getElementById("mask-container").className = "";
         itemParent.hideAll();
     }
     if (document.getElementById("navbar").className === "animated-in")
